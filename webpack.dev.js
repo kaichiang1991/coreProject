7b53fc781696d03a78b469357b927e4269900554
kai <kaichiang@smie.one>
2021-05-26 16:27:52 +0800

    把需要的js 放到webpack html 設定裡引用

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const {merge} = require('webpack-merge')
 const common = require('./webpack.common')
+const {name} = require('./package.json')
 
 module.exports = merge(common, {
     mode: 'development',
@@ -36,7 +37,19 @@ module.exports = merge(common, {
     plugins: [
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, 'dist/index.html'),
-            template: 'template.cshtml'
+            template: 'template.cshtml',
+            files: {
+                js: [
+                    'https://cdn.jsdelivr.net/npm/pixi.js-legacy@5.3.9/dist/pixi-legacy.min.js',
+                    'https://cdn.jsdelivr.net/npm/pixi-spine@2.1.14/dist/pixi-spine.min.js',
+                    'https://cdn.jsdelivr.net/npm/@pixi/sound@4.0.2/dist/pixi-sound.js',
+                    'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js',
+                    'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/EasePack.min.js',
+                    'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/PixiPlugin.min.js',
+                    'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/MotionPathPlugin.min.js',
+                    'https://cdn.jsdelivr.net/npm/number-precision@1.5.0/build/index.umd.js'
+                ].concat(['/AssetManager/index.js', '/Debug/index.js', '/EventHandler/index.js'].map(file => name + file))
+            }
         }),
 
         new CopyPlugin({
